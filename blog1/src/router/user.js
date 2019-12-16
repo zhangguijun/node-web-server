9ad8f96015d91a079c9dc24c96a2224429bcca25
zhangguijun <1725779714@qq.com>
2019-12-16 10:42:33 +0800

    【feature】新增博客用户登录接口处理

--- blog1/src/router/user.js
+++ blog1/src/router/user.js
@@ -1,13 +1,20 @@
+const { loginCheck } = require('../controller/user')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
 const handleUserRouter = (req, res) => {
   const method = req.method
 
   //  登录接口
   if (method === 'POST' && req.path === '/api/user/login') {
-    return {
-      msg: '这是登录接口'
+    const { username, password } = req.body;
+    const result = loginCheck(username, password);
+    if (result) {
+      return new SuccessModel()
+    } else {
+      return new ErrorModel('登录失败')
     }
+
   }
 
 }
 
-module.exports =  handleUserRouter
+module.exports = handleUserRouter
